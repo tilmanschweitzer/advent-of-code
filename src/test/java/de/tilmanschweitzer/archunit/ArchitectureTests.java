995b738435e03a40b7ffa3baa809036eb2501089
Tilman Schweitzer <schweitzer.tilman@gmail.com>
2022-09-02 21:36:24 +0200

    Add tests for package cycles

--- src/test/java/de/tilmanschweitzer/archunit/ArchitectureTests.java
+++ src/test/java/de/tilmanschweitzer/archunit/ArchitectureTests.java
@@ -3,16 +3,31 @@ package de.tilmanschweitzer.archunit;
 import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
 
 import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 
-@AnalyzeClasses(packages = "de.tilmanschweitzer.adventofcode")
+@AnalyzeClasses(packages = "de.tilmanschweitzer.adventofcode", importOptions = ImportOption.DoNotIncludeTests.class)
 public class ArchitectureTests {
 
 	@ArchTest
-	public void noCyclesTest(JavaClasses importedClasses) {
+	public void noCyclesInTopLevelPackages(JavaClasses importedClasses) {
 		slices().matching("de.tilmanschweitzer.adventofcode.(*)..")
 				.should().beFreeOfCycles()
+				.check(importedClasses);
+	}
+
+	@ArchTest
+	public void noCyclesInAllPackages(JavaClasses importedClasses) {
+		slices().matching("de.tilmanschweitzer.adventofcode.(**)")
+				.should().beFreeOfCycles()
+				.check(importedClasses);
+	}
+
+	@ArchTest
+	public void noDependenciesBetweenImplementations(JavaClasses importedClasses) {
+		slices().matching("de.tilmanschweitzer.adventofcode.puzzle.(*)..")
+				.should().notDependOnEachOther()
 				.check(importedClasses);
 	}
 
